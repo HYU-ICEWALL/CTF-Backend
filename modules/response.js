fc388320383b5ea3d740d365cc5673c69e575313
Lake <lake@Lakeui-Macmini.local>
2024-04-02 15:10:28 +0900

    create response class

---
+++ modules/response.js
@@ -0,0 +1,17 @@
+class Response{
+  constructor(code, message, data){
+    this.code = code;
+    this.message = message;
+    this.data = data;
+  }
+
+  response(){
+    return {
+      code: this.code,
+      message: this.message,
+      data: this.data
+    }
+  }
+}
+
+module.exports = Response;
