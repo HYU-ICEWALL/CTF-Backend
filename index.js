551d101596c23ee7eae42bf5519ff936c309733c
dongha <kdh101800@gmail.com>
2024-05-06 18:18:11 +0900

    [Feat] admin page init

--- index.js
+++ index.js
@@ -9,17 +9,21 @@ const accountRouter = require('./router/account');
 const profileRouter = require('./router/profile');
 const contestRouter = require('./router/contest');
 const problemRouter = require('./router/problem');
+const adminRouter = require('./router/admin');
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(sessionManager.session);
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.use('/api/account', accountRouter);
 app.use('/api/profile', profileRouter);
 app.use('/api/contest', contestRouter);
 app.use('/api/problem', problemRouter);
+app.use('/admin', adminRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
