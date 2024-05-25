607e680e573eb925ed8dda7cf0a58f4f1c94b3bd
dongha <kdh101800@gmail.com>
2024-05-26 03:10:28 +0900

    [Feat] page format: web page without utilities

--- router/admin.js
+++ router/admin.js
@@ -9,21 +9,73 @@ const chkIp = async (req, res, next) => {
     if(admin_ip && admin_ip !== req.ip) next();
 
     next();
-}
+};
 
 router.get('/', async (req, res) => {
     // if(!req.session || req.session.id == {ADMIN ID}){
     //     res.render('login');
     // }
 
-    res.render('base', {body: './home/index'});
-})
+    res.render('index');
+});
 
 router.get('/problems', async (req, res) => {
     // TODO: get problem sets from current contest
-    dummy = {}
+    contest = {name: "Test Contest"};
+    problems = [
+        {
+            domain: "pwn",
+            name: "test1",
+            num: "1"
+        },
+        {
+            domain: "web",
+            name: "test2",
+            num: "2"
+        },
+        {
+            domain: "forensic",
+            name: "test3",
+            num: "3"
+        },
+    ]
 
-    res.render('base', {body: './home/problems', set: dummy})
+    res.render('problems', {contest: contest, problems: problems});
+});
+
+router.get('/users', async(req, res) => {
+    const users = [
+        {
+            name: "dongha",
+            depart: "컴퓨터소프트웨어학부",
+            rank: 1,
+            score: 32
+        },
+        {
+            name: "서윤호",
+            depart: "컴퓨터소프트웨어학부",
+            rank: 2,
+            score: 27
+        }
+    ]
+    res.render('users', {users: users});
+})
+
+router.get('/contests', async(req, res) => {
+    const contests = [
+        {
+            name: "contest1",
+            people: 37,
+            date: "2024-05-25"
+        },
+        {
+            name: "contest2",
+            people: 32,
+            date: "2024-05-26"
+        }
+    ];
+
+    res.render('contests', {contests: contests});
 })
 
 
